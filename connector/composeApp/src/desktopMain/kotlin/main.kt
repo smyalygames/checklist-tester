25ef7081f30eebd204d8b56e1780199adcb92b3e
Anthony <smyalygames@gmail.com>
2024-04-01 22:54:47 +0200

    feat(connector): add window icon

--- connector/composeApp/src/desktopMain/kotlin/main.kt
+++ connector/composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,14 @@
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import connector.composeapp.generated.resources.Res
+import connector.composeapp.generated.resources.rule_24px
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalResourceApi::class)
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Checklist Tester") {
+    Window(onCloseRequest = ::exitApplication, title = "Checklist Tester", icon = painterResource(Res.drawable.rule_24px)) {
         App()
     }
 }
