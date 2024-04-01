d95305397460e6789a7067da25ed121090edf55c
Anthony <smyalygames@gmail.com>
2024-04-01 17:11:50 +0200

    feat(connector): add welcome screen

--- connector/composeApp/src/desktopMain/kotlin/Projects.kt
+++ connector/composeApp/src/desktopMain/kotlin/Projects.kt
@@ -1,7 +1,13 @@
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import connector.composeapp.generated.resources.Res
@@ -28,8 +34,29 @@ object Projects : Tab {
 
     @Composable
     override fun Content() {
-        Column {
-            Text("Projects")
+        noProjects()
+    }
+
+    @Composable
+    private fun noProjects() {
+        Column (
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Welcome!", style = MaterialTheme.typography.displayMedium)
+                Text("You currently have no projects...", style = MaterialTheme.typography.displaySmall)
+            }
+
+            Button(
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+                onClick = { /* TODO */ }
+            ) {
+                Icon(Icons.Outlined.Add, "Create New Project", modifier = Modifier.size(18.dp))
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Create Project", style = MaterialTheme.typography.labelLarge, color = MaterialTheme.colorScheme.onPrimary)
+            }
         }
     }
 }
