e3b1f6b222b75e45ae40d6ed7d5cc297075229c7
Anthony <smyalygames@gmail.com>
2024-04-02 03:53:46 +0200

    feat(connector): add about page

--- connector/composeApp/src/desktopMain/kotlin/tab/About.kt
+++ connector/composeApp/src/desktopMain/kotlin/tab/About.kt
@@ -1,8 +1,12 @@
 package tab
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import connector.composeapp.generated.resources.Res
@@ -29,8 +33,22 @@ class About : Tab {
 
     @Composable
     override fun Content() {
-        Column {
-            Text("About")
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = "The Checklist Tester is developed by Anthony Berg.",
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.size(24.dp))
+            Text("The aim of this Checklist Tester is to test checklists in a simulator " +
+                    "without the need for a pilot and provide feedback on the results of the checklist.",
+                textAlign = TextAlign.Center)
         }
     }
 }
