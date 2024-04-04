ff0bca004c83d8836a26fc0a030a9569c1385e68
Anthony <smyalygames@gmail.com>
2024-04-04 18:09:36 +0200

    build(connector): add sl4j dependencies

--- connector/shared/build.gradle.kts
+++ connector/shared/build.gradle.kts
@@ -1,14 +1,15 @@
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    kotlin("plugin.serialization").version("1.9.23")
-    id("app.cash.sqldelight").version("2.0.1")
+    kotlin("plugin.serialization") version "1.9.23"
+    id("app.cash.sqldelight") version "2.0.1"
 }
 
 val coroutinesVersion = "1.8.0"
 val ktorVersion = "2.3.9"
 val sqlDelightVersion = "2.0.1"
 val dateTimeVersion = "0.5.0"
+val sl4jVersion = "2.0.12"
 
 kotlin {
     jvm()
@@ -22,7 +23,10 @@ kotlin {
             implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
             implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
             implementation("app.cash.sqldelight:sqlite-driver:$sqlDelightVersion")
+            implementation("app.cash.sqldelight:coroutines-extensions:$sqlDelightVersion")
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
+            implementation("org.slf4j:slf4j-api:$sl4jVersion")
+            implementation("org.slf4j:slf4j-reload4j:2.0.12")
         }
     }
 }
@@ -31,6 +35,8 @@ sqldelight {
     databases {
         create("Database") {
             packageName.set("io.anthonyberg.connector.shared.database")
+            schemaOutputDirectory.set(file("src/commonMain/sqldelight/databases"))
+            deriveSchemaFromMigrations.set(true)
         }
     }
 }
