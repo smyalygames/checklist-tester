80be910456e68c7b7158554603b737bb6bddf399
Anthony <smyalygames@gmail.com>
2024-04-05 20:00:38 +0200

    feat(connector): add API endpoint for VDMJ

---
+++ connector/server/src/main/kotlin/io/anthonyberg/connector/routes/VDMJRoutes.kt
@@ -0,0 +1,23 @@
+package io.anthonyberg.connector.routes
+
+import io.anthonyberg.connector.vdmj.vdmjExecute
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.vdmjRouting() {
+    route("/vdmj") {
+        get("{exp?}") {
+            val exp = call.parameters["exp"] ?: return@get call.respondText(
+                "Missing expression",
+                status = HttpStatusCode.BadRequest,
+            )
+
+            // TODO output is empty string after first request
+            val result = vdmjExecute(exp)
+
+            call.respond(result)
+        }
+    }
+}
