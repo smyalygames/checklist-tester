e8888965e574ff9e80bf3d0ae6597b5488d4db8d
Anthony <smyalygames@gmail.com>
2024-04-06 12:10:17 +0200

    refactor(connector): follow CRUD for when to define functions, used map for getProjectById

--- connector/shared/src/commonMain/kotlin/io/anthonyberg/connector/shared/database/ProjectDatabase.kt
+++ connector/shared/src/commonMain/kotlin/io/anthonyberg/connector/shared/database/ProjectDatabase.kt
@@ -7,10 +7,37 @@ internal class ProjectDatabase (driverFactory: DriverFactory) {
     private val dbQuery = database.projectQueries
 
     /**
+     * Inserts a project into the database
+     */
+    internal fun createProject(name: String, aircraftType: String, createdUTC: String) {
+        dbQuery.createProject(
+            name = name,
+            aircraftType = aircraftType,
+            createdUTC = createdUTC,
+        )
+    }
+
+    /**
      * Gets all the Projects in the database
      */
     internal fun getAllProjects(): List<Project> {
         return dbQuery.selectAllProjects(::mapProjectSelecting).executeAsList()
+    }
+
+    /**
+     * Gets Project from the unique ID of the Project
+     * @param id Project id
+     * @return [Project]
+     */
+    internal fun getProjectById(id: Int): Project {
+        return dbQuery.selectProjectById(id.toLong(), ::mapProjectSelecting).executeAsOne()
+    }
+
+    /**
+     * Counts the amount of entries there are for projects
+     */
+    internal fun countProjects(): Long {
+        return dbQuery.countProjects().executeAsOne()
     }
 
     private fun mapProjectSelecting(
@@ -27,43 +54,5 @@ internal class ProjectDatabase (driverFactory: DriverFactory) {
             createdUTC = createdUTC,
             modifiedUTC = modifiedUTC,
         )
-    }
-
-    /**
-     * Converts database [Project](io.anthonyberg.connector.shared.database.Project) data class to entity [Project] class
-     */
-    private fun io.anthonyberg.connector.shared.database.Project.toProject() = Project(
-        id = id.toInt(),
-        name = name,
-        aircraftType = aircraftType,
-        createdUTC = createdUTC,
-        modifiedUTC = modifiedUTC,
-    )
-
-    /**
-     * Gets Project from the unique ID of the Project
-     * @param id Project id
-     * @return [Project]
-     */
-    internal fun getProjectById(id: Int): Project {
-        return dbQuery.selectProjectById(id.toLong()).executeAsOne().toProject()
-    }
-
-    /**
-     * Inserts a project into the database
-     */
-    internal fun createProject(name: String, aircraftType: String, createdUTC: String) {
-        dbQuery.createProject(
-            name = name,
-            aircraftType = aircraftType,
-            createdUTC = createdUTC,
-        )
-    }
-
-    /**
-     * Counts the amount of entries there are for projects
-     */
-    internal fun countProjects(): Long {
-        return dbQuery.countProjects().executeAsOne()
     }
 }
