c895e6aaf6098fc655e48cf013b5cc41f99b531f
Anthony <smyalygames@gmail.com>
2024-04-06 11:33:58 +0200

    feat(connector): allow database to use foreign keys

--- connector/shared/src/jvmMain/kotlin/io/anthonyberg/connector/shared/database/DriverFactory.jvm.kt
+++ connector/shared/src/jvmMain/kotlin/io/anthonyberg/connector/shared/database/DriverFactory.jvm.kt
@@ -2,10 +2,14 @@ package io.anthonyberg.connector.shared.database
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import java.util.*
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
-        val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:connector.db")
+        val url = "jdbc:sqlite:connector.db"
+        val properties = Properties().apply { put("foreign_keys", "true") }
+
+        val driver: SqlDriver = JdbcSqliteDriver(url = url, properties = properties)
         Database.Schema.create(driver)
         return driver
     }
